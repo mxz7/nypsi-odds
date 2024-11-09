6a3e1e5c4808d118b14a4707e18e6b5fd3fdfe30
max <29782397+mxz7@users.noreply.github.com>
2024-11-09 15:52:40 +0000

    show xp/money earned

--- src/utils/openCrate.ts
+++ src/utils/openCrate.ts
@@ -100,11 +100,8 @@ export function openCrate(itemId: string, found: Map<string, number>, items: { [
       const chosen = crateItemsModified[Math.floor(Math.random() * crateItemsModified.length)];
 
       if (chosen.includes("money:") || chosen.includes("xp:")) {
-        if (chosen.includes("money:")) {
-          found.set("money", found.has("money") ? found.get("money") + 1 : 1);
-        } else if (chosen.includes("xp:")) {
-          found.set("xp", found.has("xp") ? found.get("xp") + 1 : 1);
-        }
+        const [type, amount] = chosen.split(":");
+        found.set(`${type};${amount}`, found.has(chosen) ? found.get(chosen) + 1 : 1);
       } else {
         let amount = 1;
 
@@ -151,11 +148,8 @@ export function openCrate(itemId: string, found: Map<string, number>, items: { [
       const chosen = crateItems[Math.floor(Math.random() * crateItems.length)];
 
       if (chosen.includes("money:") || chosen.includes("xp:")) {
-        if (chosen.includes("money:")) {
-          found.set("money", found.has("money") ? found.get("money") + 1 : 1);
-        } else if (chosen.includes("xp:")) {
-          found.set("xp", found.has("xp") ? found.get("xp") + 1 : 1);
-        }
+        const [type, amount] = chosen.split(":");
+        found.set(`${type};${amount}`, found.has(chosen) ? found.get(chosen) + 1 : 1);
       } else {
         let amount = 1;
 
